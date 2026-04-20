4ba86c3f0302fbcce879686a0f3481afca0bbd57
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2026-04-20 15:58:35 +0530

    feat: created auth middlewaare for protect routes

---
+++ server/src/middleware/authMiddleware.js
@@ -0,0 +1,35 @@
+import jwt from 'jsonwebtoken'
+import User from '../model/userModel.js'
+
+const protect = async(req,res, next)=> {
+    try {
+        let token 
+
+        if(req.headers.authorization?.startsWith('Bearer ')){
+            token = req.headers.authorization.split(' ')[1]
+        }
+
+        if(!token){
+            return res.status(401).json({error: 'Not Authorized . No Token Provided'})
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const user = await User.findById(decoded.id)
+
+        if(!user){
+            return res.status(401).json({error: 'User not found'})
+        }
+
+        req.user = user
+        next()
+    } catch (error) {
+        if(error.name === "TokenExpiredError"){
+            return res.status(401).json({error: 'Token Expired. Please Login Again'})
+        }
+
+        return res.status(401).json({error: 'Invalid Token'})
+    }
+}
+
+
+export default protect
