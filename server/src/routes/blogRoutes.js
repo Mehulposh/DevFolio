88ad22d479dedccad65075293f29cd0520a4caf8
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2026-04-23 09:58:34 +0530

    feat: implement blog routes for managing blog posts and interactions

---
+++ server/src/routes/blogRoutes.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import {protect} from '../middleware/authMiddleware.js'
+import {
+    getBlog,
+    listAllBlogs,
+    tags,
+    stats,
+    getSlug,
+    getBlogById,
+    newBlog,
+    updateBlog,
+    deleteBlog,
+    likes
+} from '../controller/blogController.js'
+
+const router = express.Router()
+
+router.get('/', getBlog)
+router.get('/admin' , protect ,listAllBlogs)
+router.get('/tags' , tags)
+router.get('/stats' , protect , stats)
+router.get('/:slug' , getSlug)
+router.get('/admin/:id' , protect , getBlogById)
+router.post('/' , protect ,  [
+  body('title').trim().isLength({ min: 5, max: 200 }),
+  body('excerpt').trim().isLength({ min: 10, max: 500 }),
+  body('content').isLength({ min: 50 })
+], newBlog)
+
+router.put('/:id' , protect , updateBlog)
+router.delete('/:id' , protect , deleteBlog)
+router.post('/:id/like' , likes)
+
+export default router
