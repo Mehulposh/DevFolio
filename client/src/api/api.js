9d15b782e62c6943c71bdc44158e656b11f31144
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2026-04-24 16:03:42 +0530

    feat: update API base URL to production endpoint

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://devfolio-1-6kpn.onrender.com/api',
   // headers: { 'Content-Type': 'application/json' }
 });
 
