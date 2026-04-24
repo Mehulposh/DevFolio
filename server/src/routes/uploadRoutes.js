4fda0d1671de97ebf358438c12403f312ca3a0ea
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2026-04-24 16:00:55 +0530

    feat: enhance image upload route with error handling and Cloudinary configuration

--- server/src/routes/uploadRoutes.js
+++ server/src/routes/uploadRoutes.js
@@ -3,11 +3,23 @@ import {protect} from '../middleware/authMiddleware.js'
 import multer from 'multer'
 import { v2 as cloudinary } from 'cloudinary';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
-
+import dotenv from 'dotenv'
 import  {
     imageUpload,
     deleteImage,
 } from '../controller/uploadConteroller.js'
+
+dotenv.config()
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+console.log("CLOUD NAME:", process.env.CLOUDINARY_CLOUD_NAME);
+console.log("API KEY:", process.env.CLOUDINARY_API_KEY);
+console.log("API SECRET:", process.env.CLOUDINARY_API_SECRET);
 
 const storage = new CloudinaryStorage({
   cloudinary,
@@ -25,7 +37,15 @@ const upload = multer({
 
 const router = express.Router()
 
-router.post('/image' , protect , upload.single('image') , imageUpload)
+router.post('/image', protect, (req, res, next) => {
+  upload.single('image')(req, res, function (err) {
+    if (err) {
+      console.error("MULTER ERROR:", err); // ✅ THIS IS KEY
+      return res.status(500).json({ error: err.message });
+    }
+    next();
+  });
+}, imageUpload);
 router.delete('/:publicId' , protect , deleteImage)
 
 export default router
